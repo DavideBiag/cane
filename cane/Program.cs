fe7b74b35c3306b4d4cfdebc2fedba8ff109e71b
Z1 <Z1@CASA-2022>
2024-01-23 17:28:46 +0100

    Aggiungere i file di progetto.

---
+++ cane/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cane
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cane c = new Cane();
+            c.razza = "Labrador";
+            c.nome = "Ringhio";
+
+            c.proprietario = new Persona ();
+            c.proprietario.nome = "Simone";
+
+            Console.ReadLine(); 
+
+        }
+    }
+}
